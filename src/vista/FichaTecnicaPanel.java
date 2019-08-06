83ef7168e089903dd7c284d4def55c71bc880291
oscar-julian <oscar.trujillo@correounivalle.edu.co>
2019-08-06 08:08:19 -0500

    Se agrego la vista para la ficha del libro

--- src/vista/FichaTecnicaPanel.java
+++ src/vista/FichaTecnicaPanel.java
@@ -7,9 +7,9 @@ import javafx.scene.layout.GridPane;
 
 /**
  * Clase que se encarga de crear la vista una Ficha tecnica
- * @author Julian
- * Fecha de Creación: 05/08/2019
- * Fecha de ultima Modificación: 06/08/2019
+ * @author Julian 
+ * Fecha de Creación: 05/08/2019 
+ * Fecha de ultima Modificación:06/08/2019
  */
 public class FichaTecnicaPanel {
 
@@ -20,9 +20,14 @@ public class FichaTecnicaPanel {
     private final GridPane rootFicha;
     private String textoLabel;
 
+    /**
+     * Constructor de la clase
+     * @param nombreRecurso 
+     */
     public FichaTecnicaPanel(String nombreRecurso) {
         this.nombreRecurso = nombreRecurso;
         rootFicha = new GridPane();
+        rootFicha.getStylesheets().add("recursos/stylesFichaTecnica.css");
         arregloLabel = new Label[19];
         arregloTextField = new JFXTextField[15];
         arregloTextArea = new JFXTextArea[4];
@@ -30,15 +35,17 @@ public class FichaTecnicaPanel {
 
     /**
      * Metodo que carga una ficha tecnica
-     * @return 
+     * @return
      */
     public GridPane cargarFichaTecnica() {
         switch (nombreRecurso.toLowerCase()) {
             case "libro":
                 cargarFichaTecnicaLibro();
                 break;
+            case "revista":
+                cargarFichaTecnicaPeriodico();
+                break;
         }
-        desabilitarEditableText();
         return rootFicha;
     }
 
@@ -48,20 +55,27 @@ public class FichaTecnicaPanel {
     private void cargarFichaTecnicaLibro() {
         textoLabel = "Titulo,ISBN,Idioma,País de publicación,Fecha de publicación,Editorial,Edición,"
                 + "Páginas,Cubierta,Dimensiones,Signatura,Disponibilidad,Estado"
-                + ",Area,Categoria,Autores,Materia,Nota,Resumen,------";
+                + ",Area,Categoria,Autores,Materia,Nota,Resumen,";
+        
         cargarLabels(0, 10, 0, 0);
         cargarLabels(1, 7, 2, 10);
         cargarLabels(8, 9, 2, 16);
         cargarLabels(1, 2, 4, 17);
         cargarLabels(6, 7, 4, 18);
-        
-        cargarTextFields(0, 10, 1, 0);
-        cargarTextFields(1, 6, 3, 10);
-        
-        cargarTextAreas(6, 7, 3, 0);
-        cargarTextAreas(8, 9, 3, 1);
-        cargarTextAreas(1, 2, 5, 2);
-        cargarTextAreas(6, 7, 5, 3);
+
+        cargarTextFields(0, 1, 1, 0, 2);
+        cargarTextFields(1, 10, 1, 1, 1);
+        cargarTextFields(1, 6, 3, 10, 1);
+
+        cargarTextAreas(6, 7, 3, 0, 2, 1);
+        cargarTextAreas(8, 9, 3, 1, 2, 1);
+        cargarTextAreas(1, 2, 5, 2, 4, 3);
+        cargarTextAreas(6, 7, 5, 3, 4, 3);
+    }
+
+    private void cargarFichaTecnicaPeriodico() {
+        textoLabel = "Nombre,ISSN,Fecha de publicación,Ciudad,Paginas,Editorial,Disponibilidad,"
+                + "Estado fisico,Categoria";
     }
 
     /**
@@ -69,7 +83,7 @@ public class FichaTecnicaPanel {
      * @param inicioFila
      * @param limiteFila
      * @param posColumna
-     * @param posArreglo 
+     * @param posArreglo
      */
     private void cargarLabels(int inicioFila, int limiteFila, int posColumna, int posArreglo) {
         for (int i = inicioFila; i < limiteFila; i++) {
@@ -83,14 +97,13 @@ public class FichaTecnicaPanel {
      * Metodo que obtiene una palabra o frase<br>
      * de la cadena textoLabel y tambien la<br>
      * elimina
-     * @return 
+     * @return
      */
     private String obtenerFrase() {
         for (int j = 0; j < textoLabel.length(); j++) {
             if (String.valueOf(textoLabel.charAt(j)).equals(",")) {
                 String cadena = textoLabel.substring(0, j);
-                textoLabel = textoLabel.substring(j+1);
-                System.out.println(cadena);
+                textoLabel = textoLabel.substring(j + 1);
                 return cadena;
             }
         }
@@ -102,33 +115,35 @@ public class FichaTecnicaPanel {
      * @param inicioFila
      * @param limiteFila
      * @param posColumna
-     * @param posArreglo 
+     * @param posArreglo
      */
-    private void cargarTextFields(int inicioFila, int limiteFila, int posColumna, int posArreglo) {
+    private void cargarTextFields(int inicioFila, int limiteFila, int posColumna, int posArreglo, int repetirColumna) {
         for (int i = inicioFila; i < limiteFila; i++) {
-            System.out.println(posArreglo);
             arregloTextField[posArreglo] = new JFXTextField();
-            rootFicha.add(arregloTextField[posArreglo], posColumna, i);
+            rootFicha.add(arregloTextField[posArreglo], posColumna, i, repetirColumna, 1);
             posArreglo++;
         }
     }
-    
+
     /**
      * Metodo que carga los TextAreas en pantalla
+     *
      * @param inicioFila
      * @param limiteFila
      * @param posColumna
-     * @param posArreglo 
+     * @param posArreglo
      */
-    private void cargarTextAreas(int inicioFila, int limiteFila, int posColumna, int posArreglo) {
+    private void cargarTextAreas(int inicioFila, int limiteFila, int posColumna, int posArreglo, int repetirFila, int repetirColumna) {
         for (int i = inicioFila; i < limiteFila; i++) {
             arregloTextArea[posArreglo] = new JFXTextArea();
-            rootFicha.add(arregloTextArea[posArreglo], posColumna, i);
-            posArreglo++;
+            rootFicha.add(arregloTextArea[posArreglo], posColumna, i, repetirColumna, repetirFila);
         }
     }
-    
-    private void desabilitarEditableText(){
+
+    /**
+     * Metodo que restringe la entrada de datos de los TextField
+     */
+    private void desabilitarEditableText() {
         for (JFXTextField arregloTextField1 : arregloTextField) {
             arregloTextField1.setEditable(false);
         }
