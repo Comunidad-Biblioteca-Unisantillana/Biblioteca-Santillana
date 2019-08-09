3f04fec291e83c12731702d20730b3822ed04f91
oscar-julian <oscar.trujillo@correounivalle.edu.co>
2019-08-08 21:32:07 -0500

    Se eliminaron los validadores y se creo una clase que se encarga hacer las validaciones del loguin

--- src/modelo/ValidatorLogin.java
+++ src/modelo/ValidatorLogin.java
@@ -5,27 +5,32 @@ import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.validation.base.ValidatorBase;
 import javafx.scene.control.TextInputControl;
 import modeloDAO.LoginBibliotecarioDAO;
+import modeloDAO.LoginEstudianteDAO;
 
 /**
  * Clase que se encarga de verificar que los datos<br>
- * del bibliotecario existan
- * @author Julian
- * Fecha de Creación: 10/07/2019
- * Fecha de ultima Modificación: 04/08/2019
+ * del usuario existan
+ *
+ * @author Julian 
+ * Fecha de Creación: 10/07/2019 
+ * Fecha de ultima Modificación:
+ * 04/08/2019
  */
-public class ValidatorLoginBibliotecario extends ValidatorBase{
-    
-    private final JFXTextField idBibliotecario;
-    private final JFXPasswordField pwdBibliotecario;
+public class ValidatorLogin extends ValidatorBase {
+
+    private final JFXTextField codigo;
+    private final JFXPasswordField contrasena;
+    private String tipoUsuario = "";
 
     /**
      * Constructor de la clase
-     * @param idBibliotecario
-     * @param pwdBibliotecario 
+     *
+     * @param codigo
+     * @param contrasena
      */
-    public ValidatorLoginBibliotecario(JFXTextField idBibliotecario, JFXPasswordField pwdBibliotecario) {
-        this.idBibliotecario = idBibliotecario;
-        this.pwdBibliotecario = pwdBibliotecario;
+    public ValidatorLogin(JFXTextField codigo, JFXPasswordField contrasena) {
+        this.codigo = codigo;
+        this.contrasena = contrasena;
     }
 
     /**
@@ -34,16 +39,37 @@ public class ValidatorLoginBibliotecario extends ValidatorBase{
      */
     @Override
     protected void eval() {
-        if(srcControl.get() instanceof TextInputControl) evalTextField();
+        if (srcControl.get() instanceof TextInputControl) {
+            evalTextField();
+        }
     }
-    
+
     /**
      * Método que se encarga de verificar que los<br>
-     * datos de la entrada y del JFXPasswordField<br>
-     * se encuentren dentro de la base de datos
+     * datos de la entrada se encuentren dentro de<br>
+     * la base de datos
      */
-    private void evalTextField(){
-        LoginBibliotecarioDAO loginBibliotecario = new LoginBibliotecarioDAO();
-        hasErrors.set(!loginBibliotecario.readDAO(idBibliotecario.getText().trim(),pwdBibliotecario.getText().trim()));
+    private void evalTextField() {
+        switch (tipoUsuario.toLowerCase()) {
+            case "bibliotecario":
+                LoginBibliotecarioDAO loginBibliotecario = new LoginBibliotecarioDAO();
+                hasErrors.set(!loginBibliotecario.readDAO(codigo.getText().trim(), contrasena.getText().trim()));
+                break;
+            case "estudiante":
+                LoginEstudianteDAO loginEstudiante = new LoginEstudianteDAO();
+                hasErrors.set(!loginEstudiante.readDAO(codigo.getText().trim()));
+                break;
+            case "profesor":
+                hasErrors.set(!true);
+                break;
+            default:
+                System.out.println("error de tipo usuario");
+                break;
+        }
     }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
 }
