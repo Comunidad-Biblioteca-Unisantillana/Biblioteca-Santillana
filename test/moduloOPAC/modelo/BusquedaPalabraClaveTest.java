709827f4fbbcc2d63baacafa932c8d9505653732
Fabian Valencia <fabian.cordoba@correounivalle.edu.co>
2019-08-31 19:59:26 -0500

    se creo la version de pruebas de busqueda por palabra clave

---
+++ test/moduloOPAC/modelo/BusquedaPalabraClaveTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moduloOPAC.modelo;
+
+import javafx.collections.ObservableList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Stiven
+ */
+public class BusquedaPalabraClaveTest {
+    
+    public BusquedaPalabraClaveTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    /**
+     * Test of buscarRecursos method, of class BusquedaPalabraClave.
+     */
+//    @Test
+//    public void testBuscarRecursosPorPalabraClaveCorrecto() {
+//        System.out.println("buscar recursos por palabra clave");
+//        String entidad = "todos";
+//        BusquedaPalabraClave instance = new BusquedaPalabraClave("hush");
+//        ObservableList<Recurso> expResult = instance.buscarRecursos(entidad);
+//        ObservableList<Recurso> result = instance.buscarRecursos(entidad);
+//        assertEquals(expResult, result);
+//    }
+//    /**
+//     * Test of buscarRecursos method, of class BusquedaPalabraClave.
+//     */
+//    @Test
+//    public void testBuscarRecursosPorPalabraClaveErroneo() {
+//        System.out.println("buscar recursos por palabra clave");
+//        String entidad = "todos";
+//        BusquedaPalabraClave instance = new BusquedaPalabraClave("nuhd9onq09dq");
+//        ObservableList<Recurso> expResult = instance.buscarRecursos(entidad);
+//        ObservableList<Recurso> result = instance.buscarRecursos(entidad);
+//        assertEquals(expResult, result);
+//    }
+    
+}
