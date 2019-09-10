90c41b1c59021d11e48187b15362d0698127bd22
Fabian Valencia <fabian.cordoba@correounivalle.edu.co>
2019-09-09 21:22:10 -0500

    Se hizo el test del módulo de reserva de profesor

--- src/bibliotecasanti/Main.java
+++ src/bibliotecasanti/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         LoginUnisantillanaStage.getInstance();
         ConnectionBD.getInstance();
-        GeneradorMulta.getInstance().start();
+        //GeneradorMulta.getInstance().start();
     }
 
     /**
